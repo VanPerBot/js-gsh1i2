3fe98be83bcf11bbb3ead5a0503af23eb34f429f
VanPerBot <126266149+VanPerBot@users.noreply.github.com>
2023-03-03 14:58:46 +0000

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let app = new PIXI.Application({ width: 540, height: 523 });
+let app = new PIXI.Application({ width: 540, height: 523, background: 101010});
 document.body.appendChild(app.view);
 
-let sprite = PIXI.Sprite.from('sample.png');
+let sprite = PIXI.Sprite.from('https://cdn.discordapp.com/attachments/1055929322583629916/1081221412024487976/pixil-frame-0.png');
 app.stage.addChild(sprite);
 
 // import './style.css';
